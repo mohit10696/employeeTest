fbde94f1f368bf9f30420353cc3eaf48a09b0680
Mohit Sojitra <mohitsojitraa@gmail.com>
2020-11-18 13:30:29 +0530

    data binding 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome to employee app';
-
+  ManagerList = [];
+  DeveloperList = [];
+  DesignerList = [];
   onAddProjectManager(managerData:any){
-
+    this.ManagerList.push({
+      fname : managerData.fname,
+      lanme : managerData.lanme,
+      contact : managerData.contact,
+      salary : managerData.salary
+    });
+    console.log(this.ManagerList);
   }
   onAddDeveloper(developerData:any){
-
+    this.DeveloperList.push({
+      fname : developerData.fname,
+      lanme : developerData.lanme,
+      contact : developerData.contact,
+      salary : developerData.salary
+    });
+    console.log(this.DeveloperList);
   }
   onAddDesigner(designerData:any){
-
+    this.DesignerList.push({
+      fname : designerData.fname,
+      lanme : designerData.lanme,
+      contact : designerData.contact,
+      salary : designerData.salary
+    });
+    console.log(this.DesignerList);
   }
 }
